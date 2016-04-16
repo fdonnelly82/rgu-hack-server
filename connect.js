2853caaa551627dbd18c9ba2c93fdd7fb04a4395
fdonnelly82 <fdonnelly82@gmail.com>
2016-04-16 19:18:48 +0100

    database created

---
+++ connect.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Fraser on 16/04/2016.
+ */
+
+var mongodb = require('mongodb');
+
+var http = require('http');
+
+var port = process.env.PORT || 1337;
+
+var url = '  mongodb://<fdonnelly>:<drone101>@ds030829.mlab.com:30829/drone';
+
+var MongoClient = mongodb.MongoClient;
+
+http.createServer(function (request, responce) {
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+            responce.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
+            db.close();
+        }else{
+            responce.write('Connection established to' + url + "\n");
+            //database work goes here
+
+            db.close()
+        }
+        responce.end('Finished, connection closed\n');
+    })
+}).listen(port);
